59904a8fffb1c6f766f45fab5deca9f296a47be6
Danny Cho <danny.cho@appfolio.com>
2017-07-30 05:20:59 -0700

    Support statistics info

--- fetch_video_list.js
+++ fetch_video_list.js
@@ -4,7 +4,8 @@ require("dotenv").config();
 const fs = require("fs");
 const request = require("request");
 
-const yt_url_base = "https://www.googleapis.com/youtube/v3/search";
+const yt_search_url_base = "https://www.googleapis.com/youtube/v3/search";
+const yt_video_url_base = "https://www.googleapis.com/youtube/v3/videos";
 const topic_list = JSON.parse(fs.readFileSync(process.argv[2] || "topic_list.txt"));
 
 const topics = topic_list.map((topic) => {
@@ -15,15 +16,33 @@ const video_info = {};
 
 Promise.all(topics)
 .then(() => {
-  let writeStream = fs.createWriteStream("video_list.txt");
-  writeStream.write(JSON.stringify(video_info));
+
+  add_info_promises = Object.keys(video_info).map((key) => {
+    let url = yt_video_url_base + 
+    `?key=${process.env.YT_API_KEY}&id=${key}&part=statistics`;
+
+    return new Promise((resolve, reject) => {
+      request(url, (err, response, body) => {
+        video_info[key].statistics = JSON.parse(body).items[0].statistics;
+        resolve();
+      });
+    });
+  })
+
+  Promise.all(add_info_promises)
+  .then(() => {
+    let writeStream = fs.createWriteStream("video_list.txt");
+    writeStream.on("close", () => console.log("All write operations successful"));
+    writeStream.write(JSON.stringify(video_info));
+    writeStream.close();
+  })
 });
 
 
 function search_chunk(topic, resolve, nextPageToken) {
-  let url = yt_url_base +
+  let url = yt_search_url_base +
   `?key=${process.env.YT_API_KEY}&q=${topic}&pageToken=${nextPageToken || ""}`+
-  `&part=snippet&type=video&relevanceLanguage=en&videoCaption=closedCaption&maxResults=50`;
+  `&part=snippet&type=video&relevanceLanguage=en&videoCaption=closedCaption&maxResults=50&order=viewCount`;
 
   request(url, (err, res, body) => {
     if(err || !res || !body) return;
